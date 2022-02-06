bebc029628251786c9c569dbe884d5c481de0109
ARJUN PANWAR <arjun30panwar@gmail.com>
2022-02-06 22:09:02 +0530

    SirinTale

---
+++ script.js
@@ -0,0 +1,10 @@
+function sayHello(){
+	var name=document.getElementById("name").value;
+	if (name==="hello"){
+		var title=document.querySelector("#content")
+		.textContent;
+	div +="how are you"
+	document.querySelector("#content")
+	.textContent=title
+	}
+}
